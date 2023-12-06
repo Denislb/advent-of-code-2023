cd2d35e56e06a360e37f7ba3aa44294555c352e9
Denis Le Borgne <denis.leborgne@witbe.net>
2023-12-06 21:50:43 +0100

    day 6 optimize with maths

--- day_06/main.py
+++ day_06/main.py
@@ -14,6 +14,22 @@ def part_one(filename):
         ret_value *= records
     return ret_value
 
+def part_two_with_maths(filename):
+    # distance = travel_time * button_press
+    # travel_time = max_time - button_press
+    # distance = (max_time - button_press) * button_press
+    # distance = max_time * button_press - button_press * button_press
+    # -button_press * button_press + max_time * button_press - distance = 0
+    ## Quadratic equation ax^2 + bx + c = 0
+
+    import math
+    with open(filename) as f:
+        max_time = int(''.join(re.findall(r'(\d+)', f.readline())))
+        max_dist = int(''.join(re.findall(r'(\d+)', f.readline())))
+    b1 = (max_time - math.sqrt(math.pow(max_time, 2) - 4 * max_dist)) / 2
+    b2 = (max_time + math.sqrt(math.pow(max_time, 2) - 4 * max_dist)) / 2
+    return math.trunc(abs(b1 - b2))
+
 
 def part_two(filename):
     with open(filename) as f:
@@ -30,6 +46,9 @@ if __name__ == "__main__":
     input_path = "./day_06/input.txt"
     print("---Part One---")
     print(part_one(input_path))
+
+    print("---Part Two with maths---")
+    print(part_two_with_maths(input_path))
 
     print("---Part Two---")
     print(part_two(input_path))
